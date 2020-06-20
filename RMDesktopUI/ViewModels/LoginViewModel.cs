9de19e6a87f5873872424435123ab99220dd4387
Saba Gotsiridze <gotsiridze.s@outlook.com>
2020-06-21 00:35:46 +0400

    Add Login Form

---
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+		private string userName;
+		private string password;
+
+		public string UserName
+		{
+			get 
+			{
+				return userName; 
+			}
+			set 
+			{
+				userName = value;
+				NotifyOfPropertyChange(() => UserName);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+
+		public string Password
+		{
+			get 
+			{
+				return password; 
+			}
+			set 
+			{
+				password = value;
+				NotifyOfPropertyChange(() => Password);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+		public bool CanLogIn//(string userName, string password)
+		{
+			get
+			{
+				bool output = false;
+
+				if (UserName?.Length > 0 & Password?.Length > 0)
+					output = true;
+
+				return output;
+			}
+		}
+		
+		public void LogIn(string userName, string password)
+		{
+			MessageBox.Show("You are logged in");
+		}
+
+	}
+}
--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
+using RMDesktopUI.Views;
 
 namespace RMDesktopUI.ViewModels
 {
-    class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        LoginViewModel loginVM;
+
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            this.loginVM = loginVM;
+            ActivateItem(loginVM);
+        }
     }
 }
